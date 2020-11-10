7d6b5c573c7f79f61fb69f0ac66b3cbdb582233b
ELEliseev <eliseevlifter@gmail.com>
2020-11-10 11:28:08 +0300

    section 11

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import './App.scss'
+import Counter from './Counter'
+import { add, sub, addNumber, asyncAdd } from './redux/actions/actions'
+class App extends Component {
+
+  render() {
+    //console.log(this.props);
+    return (
+      <div className={'App'}>
+        <h1>Счетчик <strong>{this.props.counter}</strong></h1>
+
+        <hr />
+
+        <div className="Actions">
+          <button onClick={this.props.onAdd}>Добавить 1</button>
+          <button onClick={this.props.onSub}>Вычесть 1</button>
+        </div>
+        <div className="Actions">
+          <button onClick={() => this.props.onAddNumber(15)}>Добавить 15 </button>
+          <button onClick={() => this.props.onAddNumber(-15)}>Вычесть 15 </button>
+        </div>
+        <div className="Actions">
+          <button onClick={() => this.props.onAsyncAdd(100)}>Асинхронно добавить 100 </button>
+
+        </div>
+        <Counter />
+      </div>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+
+  return {
+    counter: state.counter1.counter
+  }
+}
+
+function mapDispathToProps(dispatch) {
+  return {
+    onAdd: () => dispatch(add()),
+    onSub: () => dispatch(sub()),
+    onAddNumber: (number) => dispatch(addNumber(number)),
+    onAsyncAdd: number => dispatch(asyncAdd(number))
+  }
+}
+
+export default connect(mapStateToProps, mapDispathToProps)(App)
